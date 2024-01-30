6dd112ba45919ebedde676ab34525d356eba2516
nathaniel-martinez <nathanielwork145@gmail.com>
2024-01-30 09:10:38 -0500

    added POSIX compliancy to linux startup script

--- env_interface/setup.sh
+++ env_interface/setup.sh
@@ -8,11 +8,32 @@
 
 psql_path=$(which psql)
 
-# This function finds the path for the command passed into it and then runs the command
-# @param: command name
+# This function finds the path for the command name passed into it and then runs the command
+# Can also filter the file paths returned based on conditional tests pased to the function
+# @param: [ -c "conditionalFunction" must be coverd in quotes ] command name
 # @outpu: command is ran after finding its path
-execute(){
-	echo -n $(find /opt -name $1; find /bin -name $1; find /usr -name $1)
+filePaths(){
+	case $1 in
+		"-c")
+			local allPaths path filteredPaths onlyPath
+			allPaths="$(find /opt -name $3; find /bin -name $3; find /usr -name $3)"
+			onlyPath="true"
+			for path in $allPaths
+			do
+				if [ $onlyPath = "true" ] && test $2 $path
+				then
+					filteredPaths=$path
+					onlyPath="false"
+				elif test $2 $path
+				then
+					filteredPaths="$filteredPaths $path"
+				fi
+			done
+			echo -n $filteredPaths
+			;;
+		*)
+			echo -n "$(find /opt -name $1; find /bin -name $1; find /usr -name $1)"
+	esac
 }
 
 
@@ -25,7 +46,7 @@ else
 	#Getting root password to run files and edit permissions
 	loginAttempt=0
 	isLoginSuccess="false"
-	loginAttempts=3
+	loginAttempts=0
 	while [ $loginAttempt -lt $loginAttempts ] && [ $isLoginSuccess = "false" ]
 	do
 		stty -echo
@@ -48,25 +69,38 @@ else
 	done
 	echo
 
-	echo "Setting up PostgreSQL data cluster"
+
+	pg_ctlPath=$(filePaths pg_ctl)
+	psqlPath=$(filePaths -c "-h" psql)
+
+
 	postgre_data="../postgre_files/postgre_data"
 	postgre_log="../postgre_files/postgre.log"
 	if [ -z "$(ls ../postgre_files/postgre_data)" ]
 	then
 		echo "Creating new local postgre cluster"
 
-		initdbPath=$(execute initdb)
-		createdbPath=$(execute createdb)
-		psqlPath=$(execute psql)
+		#initdbPath=$(filePaths initdb)
+		#createdbPath=$(filePaths createdb)
+		#psqlPath=$(filePaths psql)
+		#pg_ctlPath=$(filePaths pg_ctl)
+
+		#initdbPath=$(realpath $(filePaths initdb))
+		#createdbPath=$(realpath $(filePaths createdb))
+		#psqlPath=$(realpath $(filePaths psql))
+		#printf "\n\n Printing out real paths"
+		#echo $initdbPath
+		#echo $createdbPath
+		#echo $psqlPath
 
 		#Change file permissions on setup programs to current user
-		echo $psswd | sudo -S -k chown $USER $initdbPath 2>err.txt 1>/dev/null
-		echo $psswd | sudo -S -k chown $USER $createdbPath 2>>err.txt 1>/dev/null
-		echo $psswd | sudo -S -k chown $USER $psqlPath 2>>err.txt 1>/dev/null
+		#echo $psswd | sudo -S -k chown $USER $initdbPath 2>err.txt 1>/dev/null
+		#echo $psswd | sudo -S -k chown $USER $createdbPath 2>>err.txt 1>/dev/null
+		#echo $psswd | sudo -S -k chown $USER $psqlPath 2>>err.txt 1>/dev/null
 
 		#running setup programs 
-		$initdbPath -D $postgre_data
-		$createdbPath
+		$pg_ctlPath init -D $postgre_data
+		$pg_ctlPath -D $postgre_data -l $postgre_log start
 		$psqlPath -f ./setup.sql
 	fi
 	if [ -z $(cat /etc/group | grep -G '^postgres' | cut -f 4 -d ':' | grep -E "$USER(,(.*))?$") ]
@@ -74,7 +108,6 @@ else
 		echo "Adding user to postgres group"
 		echo $psswd | sudo -S -k usermod -a -G postgres $USER
 	fi
-	pg_ctlPath=$(execute pg_ctl)
-	$pg_ctlPath -D $postgre_data start > $postgre_log
-	$pg_ctlPath -D $postgre_data status
+	#$pg_ctlPath -D $postgre_data start > $postgre_log
+	#$pg_ctlPath -D $postgre_data status
 fi
